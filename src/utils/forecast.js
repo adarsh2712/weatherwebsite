572279d8767114e8ca94cff0a69ca390b9c5fce2
Adarsh <beingadarshshirame@gmail.com>
2020-11-12 13:06:31 +0530

    Added humidity

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -9,7 +9,7 @@ const forecast = (latitude, longitude, callback) => {
         } else if (body.error) {
             callback('Unable to find location', undefined)
         } else {
-            callback(undefined, body.current.weather_descriptions[0] + ' It is currently ' + body.current.temperature + ' degress out. but it feels like ' + body.current.feelslike + ' degrees out here.')
+            callback(undefined, body.current.weather_descriptions[0] + ' It is currently ' + body.current.temperature + ' degress out. but it feels like ' + body.current.feelslike + ' degrees out here. the Humidity is ' + body.current.humidity + '%')
         }
     })
 }
